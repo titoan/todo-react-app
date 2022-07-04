223f2b644af9b5cc82d26301bbe4c8703fc564d4
arybkin <arybkin@tmxpub.agiotg.com>
2022-07-04 23:44:50 +0300

    some todo fix

---
+++ src/component/ToDo.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+
+
+export class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      textItems: [],
+      currentTime:props.currentTime     
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.noteDown = this.noteDown.bind(this);
+    this.saveList = this.saveList.bind(this);
+    this.delItem = this.delItem.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.saveList();    
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("list")) {
+      const list = JSON.parse(localStorage.getItem("list"));
+      this.setState({ textItems: list }); 
+    }
+  }
+
+  saveList() {
+    localStorage.setItem("list", JSON.stringify(this.state.textItems));
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  noteDown(e) {
+    e.preventDefault();
+    if (this.state.value.length === 0) {
+      return;
+    }
+
+    const newItem = {
+      text: this.state.value,
+      id: Date.now(),
+    };
+
+    this.setState((state) => ({
+      textItems: state.textItems.concat(newItem), //Добавление элементов в массив должно происходить при помощи метода concat потому, что тот возвращает новый массив, а не видоизменяет текущий
+      value: "",
+    }));
+  }
+
+  delItem(e) {
+    let listItems = JSON.parse(localStorage.getItem("list"));
+    listItems = listItems.filter((el) => el.id != e.target.id);
+    this.setState({
+      textItems: listItems,
+      value: "",
+    });
+  }
+
+  clearList() {
+    localStorage.clear();
+  }
+
+  render() {
+    return (
+      <Forms
+        textItems={this.state.textItems}
+        value={this.state.value}
+        noteDown={this.noteDown}
+        handleChange={this.handleChange}
+        delItem={this.delItem}
+        currentTime={this.currentTime}
+      />
+    );
+  }
+}
+
+class Forms extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="add-list">
+        <MyNote text={this.props.textItems} currentTime={this.props.currentTime} delItem={this.props.delItem} />
+        <label>
+          <input
+            type="text"
+            name="name"
+            className="all-input"
+            value={this.props.value}
+            onChange={this.props.handleChange}
+          />
+        </label>
+        <input type="submit" value="Note down" onClick={this.props.noteDown} />
+        <input type="submit" value="Clear list" onClick={this.clearList} />
+      </form>
+    );
+  }
+}
+
+function MyNote(props) {
+  const [curTime, setCurTime] = useState(props.currentTime)
+    
+
+  return (
+    <div>
+      {props.text.map((item) => (
+        <p key={item.id} className="list-item">
+          {item.text}
+          <span>{curTime}</span>
+          <span
+            onClick={props.delItem}
+            id={item.id}
+            className="del-item"
+          ></span>
+        </p>
+      ))}
+    </div>
+  );
+}
